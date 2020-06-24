e92fbac8dc280be0d032308f103fd8d80a8dff9d
acastemoreno <acastemoreno@gmail.com>
2020-06-24 16:37:56 -0500

    webpack v1

--- src/js/holi.js
+++ src/js/holi.js
@@ -1,2 +1,1 @@
 import "../styles/holi.scss";
-console.log("holi");
--- webpack.config.js
+++ webpack.config.js
@@ -4,49 +4,62 @@ const path = require("path");
 
 let pages = ["index", "holi"];
 
-module.exports = pages.map((page) => {
-  return {
-    entry: {
-      [page]: `./src/js/${page}.js`,
-    },
-    output: {
-      filename: "js/[name].js",
-      path: path.resolve(__dirname, "dist"),
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-          },
-        },
-        {
-          test: /\.html$/,
-          use: [
-            {
-              loader: "html-loader",
-              options: { minimize: true },
+module.exports = (env, options) => {
+  return pages.map((page) => {
+    return {
+      entry: {
+        [page]: `./src/js/${page}.js`,
+      },
+      output: {
+        filename: "js/[name].js",
+        path: path.resolve(__dirname, "dist"),
+      },
+      mode: options.mode,
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
             },
-          ],
-        },
-        {
-          test: /\.(png|svg|jpg|gif)$/,
-          use: ["file-loader"],
-        },
-        {
-          test: /\.scss$/,
-          use: ["style-loader", "css-loader", "sass-loader"],
-        },
+          },
+          {
+            test: /\.html$/,
+            use: [
+              {
+                loader: "html-loader",
+                options: { minimize: true },
+              },
+            ],
+          },
+          {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+              {
+                loader: "file-loader",
+                options: {
+                  outputPath: "images",
+                },
+              },
+            ],
+          },
+          {
+            test: /\.[s]?css$/,
+            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+          },
+        ],
+      },
+      plugins: [
+        new HtmlWebPackPlugin({
+          template: `./src/${page}.html`,
+          filename: `./${page}.html`,
+        }),
+        new MiniCssExtractPlugin({
+          template: `./src/styles/${page}.html`,
+          filename: "./styles/[name].css",
+        }),
       ],
-    },
-    plugins: [
-      new HtmlWebPackPlugin({
-        template: `./src/${page}.html`,
-        filename: `./${page}.html`,
-      }),
-      new MiniCssExtractPlugin(),
-    ],
-  };
-});
+    };
+  });
+};
