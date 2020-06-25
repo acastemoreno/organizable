fe3795927c5b396f70e1cac371d515dfd9656d1c
acastemoreno <acastemoreno@gmail.com>
2020-06-25 11:57:22 -0500

    login and logout

--- src/js/boards.js
+++ src/js/boards.js
@@ -4,8 +4,14 @@ import get_protected_url from "./components/validate_auth.js";
 
 get_protected_url("http://localhost:3000/boards").then((result) => {
   if (result[0] == "error") {
-    window.location.replace("sign_up.html");
+    window.location.replace("login.html");
   } else {
     console.log("holi");
   }
+});
+
+document.querySelector("#logout").addEventListener("click", (event) => {
+  event.preventDefault();
+  localStorage.removeItem("token");
+  window.location.replace("login.html");
 });
---
+++ src/js/login.js
@@ -0,0 +1,28 @@
+import "../styles/sign_up.scss";
+
+import "@babel/polyfill";
+
+document.querySelector(".auth_form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const target = event.currentTarget;
+  const hash = {
+    username: target.querySelector("#username").value,
+    password: target.querySelector("#password").value,
+  };
+  post("http://localhost:3000/login", hash);
+});
+
+async function post(url, body) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  });
+  const data = await response.json();
+  if (!response.ok) {
+    console.log(data);
+  } else {
+    localStorage.setItem("token", data.token);
+    window.location.replace("boards.html");
+  }
+}
